8aa1b1d3aff7847fc94d6318f1644f8e3a142ad7
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-05-13 13:04:38 -0300

    Adding existing files that tom made

---
+++ win.js
@@ -0,0 +1,6 @@
+if (Meteor.is_client) {
+  Template.windows.version = "PREVIEW 0.5.9";
+  Template.windows.download = "https://dl.dropbox.com/s/jfs9iiyebu8ai9r/Meteor_PREVIEW.0.5.9.msi?dl=1";
+  Template.windows.bundle = "https://dl.dropbox.com/s/2ftlw6p2sq8t6ky/dev_bundle_MINGW32_NT-6.1_i386_0.2.23.tar.gz?dl=1";
+  Template.windows.fileSize = "12.67 MB";
+}
