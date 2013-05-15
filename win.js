215ca187a38316b9f88caccb26e7172bc8ba1dc3
Matthew Sullivan <shoebappa@gmail.com>
2013-05-15 02:30:02 -0400

    Initial draft of transition

--- win.js
+++ win.js
@@ -3,4 +3,14 @@ if (Meteor.is_client) {
   Template.windows.download = "https://dl.dropbox.com/s/jfs9iiyebu8ai9r/Meteor_PREVIEW.0.5.9.msi?dl=1";
   Template.windows.bundle = "https://dl.dropbox.com/s/2ftlw6p2sq8t6ky/dev_bundle_MINGW32_NT-6.1_i386_0.2.23.tar.gz?dl=1";
   Template.windows.fileSize = "12.67 MB";
+
+  Template.windows.sd_download = "https://docs.google.com/uc?export=download&id=0B4Vwungdd_-ReFIxSHo2enliZnM";
+  Template.windows.sd_version = "PREVIEW 0.6.2.1-rev2";
+  Template.windows.sd_fileSize = "27.5 MB";
+  Template.windows.sd_github = "https://github.com/sdarnell/meteor";
+  Template.windows.sd_wiki = "https://github.com/sdarnell/meteor/wiki/Windows";
+
+  Template.windows.pg_link = "http://goo.gl/clpKa";
+
+  Template.windows.ms_link = "https://github.com/shoebappa/vagrant-meteor-windows";
 }
