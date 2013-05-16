5a5ce09046f53fe7bee6b4b30bccc9d4c22f7f79
Stephen Darnell <stephen@darnell.plus.com>
2013-05-16 13:46:19 +0200

    Update to 0.6.3.1 MSI (link is unchanged)

--- win.js
+++ win.js
@@ -5,7 +5,7 @@ if (Meteor.is_client) {
   Template.windows.fileSize = "12.67 MB";
 
   Template.windows.sd_download = "https://docs.google.com/uc?export=download&id=0B4Vwungdd_-RWVR3RXBVVGw0dTA";
-  Template.windows.sd_version = "PREVIEW 0.6.3";
+  Template.windows.sd_version = "PREVIEW 0.6.3.1";
   Template.windows.sd_fileSize = "27.5 MB";
   Template.windows.sd_github = "https://github.com/sdarnell/meteor";
   Template.windows.sd_wiki = "https://github.com/sdarnell/meteor/wiki/Windows";
