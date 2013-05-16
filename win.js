e6175c0dc2b8b0ac6eaecbc4ab6b1c6246558756
Stephen Darnell <stephen@darnell.plus.com>
2013-05-16 12:43:44 +0200

    Update links to 0.6.3 MSI

--- win.js
+++ win.js
@@ -4,8 +4,8 @@ if (Meteor.is_client) {
   Template.windows.bundle = "https://dl.dropbox.com/s/2ftlw6p2sq8t6ky/dev_bundle_MINGW32_NT-6.1_i386_0.2.23.tar.gz?dl=1";
   Template.windows.fileSize = "12.67 MB";
 
-  Template.windows.sd_download = "https://docs.google.com/uc?export=download&id=0B4Vwungdd_-ReFIxSHo2enliZnM";
-  Template.windows.sd_version = "PREVIEW 0.6.2.1-rev2";
+  Template.windows.sd_download = "https://docs.google.com/uc?export=download&id=0B4Vwungdd_-RWVR3RXBVVGw0dTA";
+  Template.windows.sd_version = "PREVIEW 0.6.3";
   Template.windows.sd_fileSize = "27.5 MB";
   Template.windows.sd_github = "https://github.com/sdarnell/meteor";
   Template.windows.sd_wiki = "https://github.com/sdarnell/meteor/wiki/Windows";
