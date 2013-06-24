a78b22d4a255a7df985d9109e52b0cf8b13107bc
Stephen Darnell <stephen@darnell.plus.com>
2013-06-24 10:13:11 +0200

    Switch to dropbox download link

--- win.js
+++ win.js
@@ -7,7 +7,7 @@ if (Meteor.is_client) {
   Template.windows.meteor_link = "http://www.meteor.com/";
   Template.windows.meteorite_link = "https://github.com/oortcloud/meteorite";
 
-  Template.windows.sd_download = "https://copy.com/4Oh7ybf07e6I/Meteor_0.6.4-rev1.msi?download=1";
+  Template.windows.sd_download = "https://dl.dropboxusercontent.com/s/itlj5n5wnbna1lz/Meteor_0.6.4-rev1.msi";
   Template.windows.sd_version = "0.6.4-rev1";
   Template.windows.sd_fileSize = "27.7 MB";
   Template.windows.sd_github = "https://github.com/sdarnell/meteor";
